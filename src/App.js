7beee9810e5d208644600e179d8b53f9fb0bfaaa
Stephan D <duenkel.stephan@gmail.com>
2019-06-24 22:56:19 +0200

    function changed to class, added constructor and lifecycle methods. #2

--- src/App.js
+++ src/App.js
@@ -11,42 +11,78 @@ import {
   ListGroupItem
 } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App container">
-      <h1>Produkt</h1>
-      <ListGroup>
-        <ListGroupItem>
-          <Card>
-            <CardTitle>Produkt Name</CardTitle>
-            <Button>Editieren</Button>
-            <Button>Löschen</Button>
-            <CardBody>
-              <CardSubtitle>Beschreibung</CardSubtitle>
-              <CardText>
-                Das ist die Produktbeschreibung des Produktes.
-              </CardText>
-              <CardText>Preis: 1 €</CardText>
-            </CardBody>
-          </Card>
-        </ListGroupItem>
-        <ListGroupItem>
-          <Card>
-            <CardTitle>Produkt Name 2</CardTitle>
-            <Button>Editieren</Button>
-            <Button>Löschen</Button>
-            <CardBody>
-              <CardSubtitle>Beschreibung</CardSubtitle>
-              <CardText>
-                Das ist die Produktbeschreibung des Produktes.
-              </CardText>
-              <CardText>Preis: 2 €</CardText>
-            </CardBody>
-          </Card>
-        </ListGroupItem>
-      </ListGroup>
-    </div>
-  );
+/**
+ * The App.
+ */
+class App extends React.Component {
+  
+  /**
+   * Constructor of the App.
+   *
+   * @param props Has the attributes of the app tag
+   */
+  constructor(props) {
+    super(props);
+  }
+
+  /**
+   * After the Component output has been rendered to the DOM.
+   */
+  componentDidMount() {
+
+  }
+
+  /**
+   * After the component is being removed from the DOM.
+   */
+  componentWillUnmount() {
+
+  };
+
+  render() {
+    return (
+      <div className="App container">
+        <h1>Produkt - {this.props.name}</h1>
+        <ListGroup>
+          <ListGroupItem>
+            <Card>
+              <CardTitle>Produkt Name</CardTitle>
+              <Button>Editieren</Button>
+              <Button>Löschen</Button>
+              <CardBody>
+                <CardSubtitle>Beschreibung</CardSubtitle>
+                <CardText>
+                  Das ist die Produktbeschreibung des Produktes.
+                </CardText>
+                <CardText>Preis: 1 €</CardText>
+              </CardBody>
+            </Card>
+          </ListGroupItem>
+          <ListGroupItem>
+            <Card>
+              <CardTitle>Produkt Name 2</CardTitle>
+              <Button>Editieren</Button>
+              <Button>Löschen</Button>
+              <CardBody>
+                <CardSubtitle>Beschreibung</CardSubtitle>
+                <CardText>
+                  Das ist die Produktbeschreibung des Produktes.
+                </CardText>
+                <CardText>Preis: 2 €</CardText>
+              </CardBody>
+            </Card>
+          </ListGroupItem>
+        </ListGroup>
+      </div>
+    );
+  }
+}
+
+/**
+ * Define default props.
+ */
+App.defaultProps = {
+  name: "test"
 }
 
 export default App;
